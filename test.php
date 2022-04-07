af2a536d1a273f9fc1261f3e55b5d4241e2abc8f
Jonathan Leo Qi Xiang <jonathanleoqixiang@gmail.com>
2022-04-07 21:58:03 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,2 @@
-<html>
-  <head>
-    <title>Online PHP Script Execution</title>
-  </head>
-  <body>
-    <?php
-       echo "<h1>Hello, PHP!</h1>\n";
-    ?>
-  </body>
-</html>
+<?php
+  echo "<h1>Hello, PHP!</h1>\n";
