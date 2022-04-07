8088a1afceec5d308302c39c5d07ac13c8f8739b
Jonathan Leo Qi Xiang <jonathanleoqixiang@gmail.com>
2022-04-07 19:39:33 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,12 @@
-<?php echo "hello"; ?>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
